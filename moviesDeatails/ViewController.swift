7b29200895e88f9a9c996b7e495560b0762fbda9
Anshuman Bhargava <anshuman.bhargava@housing.com>
2023-08-22 14:04:27 +0530

    Initial Commit

---
+++ moviesDeatails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moviesDeatails
+//
+//  Created by Anshuman Bhargava on 22/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
