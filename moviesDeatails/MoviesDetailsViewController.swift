f4a237f024c6aed1f5ffee1e1a93b457c0890c83
Anshuman Bhargava <anshuman.bhargava@housing.com>
2023-08-23 15:12:11 +0530

    Completed

--- moviesDeatails/MoviesDetailsViewController.swift
+++ moviesDeatails/MoviesDetailsViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class MoviesDetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let c = ["King", "Queen", "Rook", "Bishop", "Knight", "Pawn","King", "Queen", "Rook", "Bishop", "Knight", "Pawn","King", "Queen", "Rook", "Bishop", "Knight", "Pawn","King", "Queen", "Rook", "Bishop", "Knight", "Pawn"]
+
     var moviesList = [MovieData]()
     var currentIndex: Int?
     @IBOutlet weak var MoviesDetailsTableView: UITableView!
@@ -20,7 +20,6 @@ class MoviesDetailsViewController: UIViewController, UITableViewDelegate, UITabl
         MoviesDetailsTableView.dataSource = self
         MoviesDetailsTableView.delegate = self
         getMoviesListFromApi()
-        print(moviesList.count)
     }
     
     func getMoviesListFromApi(){
@@ -52,14 +51,17 @@ class MoviesDetailsViewController: UIViewController, UITableViewDelegate, UITabl
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return c.count
+        return moviesList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = MoviesDetailsTableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as? MoviesDetailsTableViewCell
         
-        let imageURL = URL(string: "https://image.tmdb.org/t/p/w500//6oH378KUfCEitzJkm07r97L0RsZ.jpg")
-        //cell?.movieImage.image = Downloader.imageDownloaderUrlSession(fromurl: "https://image.tmdb.org/t/p/w500//6oH378KUfCEitzJkm07r97L0RsZ.jpg")
+        let urlString = String("https://image.tmdb.org/t/p/w500" + moviesList[indexPath.row].poster_path)
+        cell?.movieTitle.text = "\(String(moviesList[indexPath.row].title))"
+        cell?.movieOverview.text = "\(String(moviesList[indexPath.row].overview))"
+        let imageURL = URL(string: urlString)
         URLSession.shared.downloadTask(with: imageURL!){ (fileURL, URLResponse, error) in
             guard error == nil else{
                 print(error!.localizedDescription)
@@ -78,14 +80,6 @@ class MoviesDetailsViewController: UIViewController, UITableViewDelegate, UITabl
                 cell?.movieImage.layer.cornerRadius = 15
             }
         }.resume()
-        
-        cell?.movieTitle.text = c[indexPath.row]
-        cell?.movieOverview.text = "On the brink of losing her childhood home, Maddie discovers an intriguing job listing: wealthy helicopter parents looking for someone to “date” their introverted 19-year-old son, Percy, before he leaves for college. To her surprise, Maddie soon discovers the awkward Percy is no sure thing."
-        
-//        let urlString = String("https://image.tmdb.org/t/p/w500" + moviesList[indexPath.row].poster_path)
-//        cell?.movieImage.image = Downloader.imageDownloader(fromUrl: urlString)
-//        cell?.movieTitle.text = "\(String(moviesList[indexPath.row].title))"
-//        cell?.movieOverview.text = "\(String(moviesList[indexPath.row].overview))"
         return cell!
     }
     
@@ -97,7 +91,7 @@ class MoviesDetailsViewController: UIViewController, UITableViewDelegate, UITabl
     
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "currentIndex" {
+        if segue.identifier == "movieDetailsSegue" {
             let movieContent = segue.destination as? MoviesOverviewViewController
             movieContent?.movieDetail = moviesList[currentIndex!]
         }
--- moviesDeatails/MoviesOverviewViewController.swift
+++ moviesDeatails/MoviesOverviewViewController.swift
@@ -26,28 +26,19 @@ class MoviesOverviewViewController: UIViewController {
     
     func setAllDetails(){
         moviePoster.layer.cornerRadius = 15
-        movieTitle.text = "Elemental"
-        moviePoster.image = Downloader.imageDownloader(fromUrl: "https://image.tmdb.org/t/p/w500//6oH378KUfCEitzJkm07r97L0RsZ.jpg")
-        moviePopularity.text = "3816.744"
-        movieRating.text = "7.8"
-        movieReleaseDate.text = "2023-06-14"
-        movieOverview.text = "In a city where fire, water, land and air residents live together, a fiery young woman and a go-with-the-flow guy will discover something elemental: how much they have in common."
-    }
-    func setAllDetail(){
-        moviePoster.layer.cornerRadius = 15
         movieTitle.text = movieDetail?.title
         moviePoster.image = Downloader.imageDownloader(fromUrl: "https://image.tmdb.org/t/p/w500" + movieDetail!.poster_path)
-        moviePopularity.text = String(describing: movieDetail?.popularity)
-        movieRating.text = String(describing: movieDetail?.vote_average)
+        moviePopularity.text = "\(movieDetail!.popularity)"
+        movieRating.text = "\(movieDetail!.vote_average)"
         movieReleaseDate.text = movieDetail?.release_date
         movieOverview.text = movieDetail?.overview
     }
     
     func backButton(){
-        movieOverview.text = " On the brink of losing her childhood home, Maddie discovers an intriguing job listing: wealthy helicopter parents looking for someone to “date” their introverted 19-year-old son, Percy, before he leaves for college."
         let backButton = UIBarButtonItem()
         backButton.title = "Back"
         self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
     }
 
 }
+
